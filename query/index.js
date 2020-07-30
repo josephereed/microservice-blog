fb6f9f5a068b7f196cf17eac9cb1eaf34b369e56
Joseph Reed <jereed559@gmail.com>
2020-07-30 04:54:49 -0700

    initial commit

---
+++ query/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId, status } = data;
+    const post = posts[postId];
+    post.comments.push({ id, content, status });
+  }
+  console.log(posts);
+  res.send({});
+});
+
+app.listen(4002, () => {
+  console.log('query service listening on port 4002');
+});
